25274ef2cfb19433ff4a2554c5e58ef429512686
Ash Srinivas <ash@supallama.ai>
2024-07-09 01:42:05 -0700

    Created a new repo from a template via GitHub API & Celery task & FastAPI

--- app/routes/github/tasks.py
+++ app/routes/github/tasks.py
@@ -1,6 +1,10 @@
 from celery import shared_task
 
+import requests
+
 from typing import List
+
+from app.config import settings
 
 """Define as shared_task instead of celery.task 
    to avoid circular imports, and allow this 
@@ -17,7 +21,27 @@ def create_repos_from_templates_task(app_prefix: str, repo_urls: List[str]) -> s
     print("********** Inside create repos task! ************")
     print(f"app_prefix: {app_prefix}")
     print(f"repo_urls: {repo_urls}")
+    
+    github_token = settings.GITHUB_PERSONAL_ACCESS_TOKEN
 
-    import time
-    time.sleep(2)
-    return 'forked!'
+    TEMPLATE_OWNER = "SupaLlama"
+    TEMPLATE_REPO = "supallama-worker-starter" 
+
+    url = f"https://api.github.com/repos/{TEMPLATE_OWNER}/{TEMPLATE_REPO}/generate"
+    print("url", url)
+    data = { 
+        "owner": "supallama",
+        "name": "ash-created-this-repo-from-a-template",
+        "private": True
+    }
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {github_token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    print("status code", response.status_code)
+    print("json response", response.json())
+
+    return "created?!"
