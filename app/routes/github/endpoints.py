5b4c47f3785e847ccb732730cdcdb14e70f3e862
Ash Srinivas <ash@supallama.ai>
2024-07-10 16:44:01 -0700

    Cleaned up schema, task and endpoint for template repo creation

--- app/routes/github/endpoints.py
+++ app/routes/github/endpoints.py
@@ -14,6 +14,6 @@ from .tasks import create_repos_from_templates_task
 @github_router.post("/create-repos-from-templates")
 def create_repos_from_templates(request_body: CreateReposFromTemplatesBody):
     task = create_repos_from_templates_task.delay(
-        request_body.app_prefix, request_body.repo_template_urls
+        request_body.app_prefix
     )
     return JSONResponse({"task_id": task.task_id})
